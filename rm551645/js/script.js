45df08d840211b3b95c1119a8e1336b1bfe096b1
bdancs <rm551645@fiap.com.br>
2023-03-21 11:57:56 -0300

    Criado arquivo js, evento + declarações

---
+++ rm551645/js/script.js
@@ -0,0 +1,26 @@
+
+
+//Capturando a tag com ID e colocando em uma variável var
+var h1Elemento = document.getElementById("titulo-1")
+
+//Adicionando um Listener de Evento
+h1Elemento.addEventListener("click", function(){
+
+    alert("Agora sou autônomo!")
+
+})
+
+//Declaração de variáveis com LET e VAR e CONST
+
+var nome = "Pedro"
+
+if(nome != ""){
+    let nome = "Emanuelle"
+}
+
+//Imprimindo o valor da variável
+console.log(nome)
+
+//let nome = "Pedro"
+//const nome = "Pedro"
+
