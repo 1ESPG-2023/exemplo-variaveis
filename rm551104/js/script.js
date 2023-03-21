1c9fc80124984aebf177da3321a43867922e235a
fernandakaory <rm551104@fiap.com.br>
2023-03-21 11:56:51 -0300

    var let e const

---
+++ rm551104/js/script.js
@@ -0,0 +1,25 @@
+
+
+//capturando a tag com ID
+var h1Elemento = document.getElementById("titulo-1")
+
+//adicionando um listener de evento
+h1Elemento.addEventListener("click",function(){
+  
+  alert("Agora sou autonômo")
+
+})
+
+//declaração de variáveis com let var e const
+
+//var é global
+let nome="Pedro"
+if(nome!=""){
+    let nome="Fernanda"
+}
+//imprimindo valor da variavel
+console.log(nome)
+
+
+
+ 
