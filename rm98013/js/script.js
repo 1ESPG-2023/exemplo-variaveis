8e0f5dd0b15502d05e31b4893ebfd8f3288b02fa
Rodrigo <rm98013@fiap.com.br>
2023-03-21 12:17:00 -0300

    formas de utilização de javascript e eventos

---
+++ rm98013/js/script.js
@@ -0,0 +1,25 @@
+
+
+//capturando a tag com ID e colocando em uma variável var
+var h1Elemento = document.getElementById("titulo-1")
+
+//adicionando um listener de Evento
+h1Elemento.addEventListener("click", function(){
+   
+    alert("Agora sou autonômo!")
+    
+})
+
+//declaração de variaveis com LET e VAR e CONST
+
+//const nome = "Pedro"
+
+var nome = "Pedro"
+
+if(nome != ""){
+    var nome = "Emanuelle"
+}
+
+//imprimindo o valor da variavel
+
+console.log();
