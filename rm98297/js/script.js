1b75e8bd058805c91912e62997124fde22fbaae0
lucassperanzini <RM98297@fiap.com.br>
2023-03-21 11:57:05 -0300

    Exemplo de ultilização do JSM+obj document.

---
+++ rm98297/js/script.js
@@ -0,0 +1,23 @@
+
+// Capturando a tag com ID e colocando em uma variável var
+var h1Elemento = document.getElementById("titulo-1")
+
+
+//Adicionando um Listener de Evento
+h1Elemento.addEventListener("click",function(){
+
+    alert("Agora sou autônomo!!")
+})
+
+
+// Declaração de variaves com LET e VAR e CONST
+
+
+var nome = "Pedro"
+
+if(nome != ""){
+    let nome = "Emenuelle"
+}
+
+//imprimindo o valor de variável
+console.log(nome)
