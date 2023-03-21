a9ac1c9df81ba6b929e9a731763626ad85f43de1
MirellaRossi <rm97990@fiap.com.br>
2023-03-21 11:56:13 -0300

    adicionado funções alert e declarada varavel nome

---
+++ rm97990/js/script.js
@@ -0,0 +1,20 @@
+
+
+//Capturando a tag com id e colocando em uma variável var
+var h1Elemento = document.getElementById("titulo 1")
+//adicionando um listener de eventos
+h1Elemento.addEventListener("click",function(){
+    alert("Agora sou autonômo")
+})
+
+//declaração de variáveis com Let e var e const
+//const nome ="Mirella"
+
+var nome = "Mirella"
+ if(nome != ""){
+    let nome = "Pedro"
+
+ }
+ console.log(nome)
+
+ 
