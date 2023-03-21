f39c5dadfa38de7624d9680e00e455255ea75b62
ViniciusBarrosFiap <rm97824@fiap.com.br>
2023-03-21 11:57:56 -0300

    variaveis var, let, const

--- rm97824/js/script.js
+++ rm97824/js/script.js
@@ -5,3 +5,11 @@ var h1Elemento = document.getElementById("title-1")
 h1Elemento.addEventListener("click", function(){
     alert("Agora sou autonomo")
 })
+
+//declaração de variaveis com let, var e const
+
+var nome = "Vinicius"  //var é mutavel
+
+let nome = "Vinicius" //let não é mutavel
+
+const nome = "Vinicius" //const é uma constante
