fd243b8a4b0182db6e0de5d5874c349ba88ebd71
victoriafpizza <rm550609@fiap.com.br>
2023-03-21 11:56:23 -0300

    ex de utilização do js + obj document.

---
+++ rm550609/js/script.js
@@ -0,0 +1,20 @@
+//capturando a tag com id e colocando em uma variavel var
+var h1Elemento = document.ELementById("titulo-1")
+
+//adicionar um Listener de evento
+h1Elemento.addEventListener("click",function(){
+
+    alert("agora sou autonomo!")
+
+})
+
+//declaração com let e var const
+
+var nome = "Pedro"
+
+if(nome != ""){
+    var nome "Emanuelle"
+}
+
+//imprimindo o valor da variavel 
+console.log(nome)
