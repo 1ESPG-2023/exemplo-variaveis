ac65d1e05f574181dba9bc1bb97ebfc6f53d0108
Gava221 <rm550595@fiap.com.br>
2023-03-21 11:56:40 -0300

    trabai

---
+++ rm550595/js/script.js
@@ -0,0 +1,16 @@
+
+//capturando a tag com ID e colocando em uma variavel var
+var h1Elemento = document.getElementById("Titulo 1")
+
+//adicionando listener de evento
+h1Elemento.addEventListener("click",function(){
+
+    alert:("agora sou autonomo!")
+
+})
+
+//var nome = "pedro"
+
+//if(nome  ""){
+    //var nome = "Emanuelle"
+//}
