e2a4cf0ad1a810532f8332df804a472ffd705fb6
LanaAuada <rm551143@fiap.com.br>
2023-03-21 11:58:33 -0300

    aula

---
+++ rm551143/js/script.js
@@ -0,0 +1,24 @@
+
+
+//capturando a tag com ID e colocando em uma variável var
+var h1Elemento = document.getElementById("titulo-1")
+
+//adicionando um listener de evento
+h1Elemento.addEventListener("click",function(){
+
+    alert("agora sou autônomo")
+
+})
+
+//declaração de variáveis com LET e VAR e CONST
+
+//const nome = "Pedro"
+
+var nome = "Pedro"
+if(nome !=""){
+    var nome = "Lana"
+}
+
+//imprimindo o valor da variável
+console.log(nome)
+
