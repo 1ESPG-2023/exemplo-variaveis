1c387d94c4a5c3a1da285455b8811a23828b4792
kanvzt0 <rm98919@fiap.com.br>
2023-03-21 11:59:04 -0300

    Criado arquivo JS + evento

---
+++ pf0670/script.js
@@ -0,0 +1,20 @@
+
+
+//Capturando a tag com ID e colocando em uma variável var
+var h1Elemento = document.getElementById("titulo-1")
+
+//Adicionando um Listener de Evento
+h1Elemento.addEventListener("click",function(){
+   alert("Agora eu sou autonomo")
+})
+
+//Declaração de variáveis com LET e VAR e CONST
+
+const nome = "Pedro"
+var nome ="Pedro"
+let nome = "Pedro"
+
+if(false)(
+    var nome = "Emanuelle"
+)
+
