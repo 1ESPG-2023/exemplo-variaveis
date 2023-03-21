102a0cc4c5a1d44dd150c89c9484cd1e1812c34c
BernardoPRocha <RM99209@fiap.com.br>
2023-03-21 12:00:05 -0300

    JS do site

---
+++ rm99209/js/script.js
@@ -0,0 +1,23 @@
+
+
+//Capturando a tag com ID e colocando em uma variável var 
+var h1Elemento = document.getElementById("título-1")
+
+//adicionando um listener de Evento
+h1Elemento.addEventListener("click", function(){
+    alert("Título 1") 
+})
+
+//Declaração de variáveis com LET, VAR e CONST 
+
+//const noMe = "Pedro"
+var nome = "Pedro"
+
+if(nome != ""){
+    var nome = "Bernardo"
+}
+
+console.log(nome)
+
+
+    
