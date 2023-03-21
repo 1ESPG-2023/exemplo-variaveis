7361f6b2f513c470fa22bb2a2b01532faa8f7ff3
MachadONLY <rm551570@fiap.com.br>
2023-03-21 11:58:00 -0300

    Criado arquivo js evento + variaveis let e const

---
+++ js/script.js
@@ -0,0 +1,24 @@
+
+
+//Capturando a tag com ID
+var h1Elemento = document.getElementById("Titulo-1")
+
+//adicionando um Listener de Evento
+h1Elemento.addEventListener("click",function(){
+
+    alert("Agora sou autonômo")
+})
+
+//Declaração de Variaveis com LET e VAR e CONST
+
+//const nome = "Pedro"
+
+var nome = "Pedro"
+
+if(nome != ""){}
+    var nome = "Lucas"
+
+
+
+
+
