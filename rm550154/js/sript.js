fa930f9efc9580737067ba47eec1e89a5a779005
KevinBeykam <rm550154@fiap.com.br>
2023-03-21 11:59:31 -0300

    java variaveis

---
+++ rm550154/js/sript.js
@@ -0,0 +1,21 @@
+
+
+//Capturano a tag com ID é colocando em uma variável var 
+var h1Elemento = document.getElementById("titulo-1")
+//Adicionando um Listener de Evento
+h1Elemento.addEventListener("click",function(){
+
+    alert("Agora sou autonômo")
+
+})
+
+//Declaração de variáveis com LET e VAR e CONST
+ //const nome = "Pedro"
+ var nome = "Pedro"
+ 
+ if(nome != ""){
+    var nome = "Emanuelle"
+ }
+
+ //Imprimino o valor da variável
+ console.log(nome)
