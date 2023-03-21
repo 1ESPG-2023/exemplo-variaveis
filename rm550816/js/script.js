e42b5a6550c9cdc26fe2da11f3b8fffa5cbc0976
Rodrigooou <rm550816@fiap.com.br>
2023-03-21 11:56:37 -0300

    Exemplo de utilização do JS

---
+++ rm550816/js/script.js
@@ -0,0 +1,21 @@
+
+
+//Capturando a tag com ID e colocando em uma variável var
+var h1Elemento = document.getElementById("titulo-1")
+
+//Adicionando um Listener de Evento
+h1Elemento.addEventListener("click", function(){
+
+
+alert("Agora sou autonomo")
+
+
+})
+
+//Declaração de variáveis com LET e VAR e CONST
+var nome = "Pedro"
+
+if(nome != ""){
+    var name ="rodrigo"
+}
+console.log(name)
