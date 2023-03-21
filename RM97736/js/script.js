1d7b613b6d7ad43b190d28d5c42a4ee4902a40bc
geovannaacunha <rm97736@fiap.com.br>
2023-03-21 11:55:39 -0300

    aula 21/03

---
+++ RM97736/js/script.js
@@ -0,0 +1,21 @@
+
+
+//Capturando a tag com ID e colocando em uma variavel var
+var h1Elemento = document.getElementById("título-1")
+
+// Adicionando um listener de evento 
+h1Elemento.addEventListener("click", function(){
+    alert("Agora sou autônomo")
+})
+
+// Declaração de variaveis com LET e VAR e CONST
+
+var nome = "Pedro"
+//let nome = "Pedro"
+// const nome = "Pedro"
+if(nome != ""){
+    let nome = "Emanuelle"
+}
+
+//Imprimindo o valor da variavel
+console.log(nome)
