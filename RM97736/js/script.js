7d6dca1896b19a4762234fe9b95046d345fd0bb8
geovannaacunha <rm97736@fiap.com.br>
2023-03-28 11:48:30 -0300

    Operadores

--- RM97736/js/script.js
+++ RM97736/js/script.js
@@ -1,31 +1,59 @@
 
 
-//Capturando a tag com ID e colocando em uma variavel var
-var h1Elemento = document.getElementById("título-1")
+// //Capturando a tag com ID e colocando em uma variavel var
+// var h1Elemento = document.getElementById("título-1")
 
-// Adicionando um listener de evento 
-h1Elemento.addEventListener("click", function(){
-    alert("Agora sou autônomo")
-    h1Elemento.innerHTML = "NOVO TÍTULO"
-    h1Elemento.style.backgroundColor = "#ff0000"
-})
+// // Adicionando um listener de evento 
+// h1Elemento.addEventListener("click", function(){
+//     alert("Agora sou autônomo")
+//     h1Elemento.innerHTML = "NOVO TÍTULO"
+//     h1Elemento.style.backgroundColor = "#ff0000"
+// })
 
-h1Elemento.addEventListener("mouseover", ()=>{
-    h1Elemento.style.fontSize = "68px"
-})
+// h1Elemento.addEventListener("mouseover", ()=>{
+//     h1Elemento.style.fontSize = "68px"
+// })
 
-h1Elemento.addEventListener("mouseleave", ()=>{
-    h1Elemento.style.fontSize = "34px"
-})
+// h1Elemento.addEventListener("mouseleave", ()=>{
+//     h1Elemento.style.fontSize = "34px"
+// })
 
-// Declaração de variaveis com LET e VAR e CONST
+// // Declaração de variaveis com LET e VAR e CONST
 
-var nome = "Pedro"
-//let nome = "Pedro"
-// const nome = "Pedro"
-if(nome != ""){
-    let nome = "Emanuelle"
-}
+// var nome = "Pedro"
+// //let nome = "Pedro"
+// // const nome = "Pedro"
+// if(nome != ""){
+//     let nome = "Emanuelle"
+// }
 
-//Imprimindo o valor da variavel
-console.log(nome)
+// //Imprimindo o valor da variavel
+// console.log(nome)
+
+
+
+// estrutura de decisão if\else\ else if
+//Operadores lógicos
+//&& == and
+// !! == or
+// ! == not/different
+// let nome = ""
+// console.log(nome)
+
+// if(nome != "" && nome != undefined){
+//     console.log("A variável está preenchida")
+// }else if(nome == null){
+//     console.log("A variável é null")
+// }
+// else{
+//     console.log("A variável não está preenchida")
+// }
+
+
+//operador ternário
+let null1 = 10;
+let null2 = 5;
+
+//crie uma validação ternária onde resulte emo valor de PAR ou IMPAR
+let resultado = !(10%5) ? "PAR" : "IMPAR";
+console.log("o número que está sendo manipulado é: " + resultado)
