9ea4eda46310eabe2f177319fb631dc47c714511
GabrielMachadoCP <rm99880@fiap.com.br>
2023-03-28 11:48:21 -0300

    Trabalhamos com operador ternário

--- 1rm99880/Js/script.js
+++ 1rm99880/Js/script.js
@@ -29,3 +29,50 @@
 
 // //Imprimindo o nome para ser impresso
 // console.log(nome)
+
+
+
+
+//Estrutura de decisão IF / else / else if
+//Operadores lógicos
+//&& AND
+//|| OR
+// ! NOT
+// !=  NOT / DIFERENTE
+
+// let nome = null
+// console.log(nome)
+
+// if (nome != "" && nome != undefined){
+//     console.log("A variável está preenchida!")
+// }
+// else if(nome == null){
+//     console.log("A variável é null")
+// }
+// else{
+//     console.log("A variável não está preenchida!")
+//     }
+
+
+
+
+
+//Operadores Relacionais
+// > Maior
+// < Menor
+// >= Maior ou igual
+// <= Menor ou igual
+
+
+
+
+
+
+//Operador Ternário ( ? )
+
+let num1 = 10;
+let num2 = 5;
+//crie uma validação ternária onde resulte no valor de PAR ou ÍMPAR!
+
+let resultado = !(10%5) ? "PAR" : "ÍMPAR";
+console.log("O número que está sendo manipulado é: "+ resultado)
