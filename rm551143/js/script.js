9f0d0f9ddbbade4b8c960bbc79c99b0f502d9f1d
LanaAuada <rm551143@fiap.com.br>
2023-03-28 11:10:00 -0300

    funções listener

--- rm551143/js/script.js
+++ rm551143/js/script.js
@@ -1,14 +1,25 @@
-
-
-//capturando a tag com ID e colocando em uma variável var
-var h1Elemento = document.getElementById("titulo-1")
+/* //capturando a tag com ID e colocando em uma variável var
+const h1Elemento = document.getElementById("titulo-1")
 
 //adicionando um listener de evento
+//ponto para alterar propriedades
 h1Elemento.addEventListener("click",function(){
 
     alert("agora sou autônomo")
+    h1Elemento.innerHTML = "NOVO TÍTULO!";
+//h1Elemento.textContent = "NOVO TÍTULO!";
+    h1Elemento.style.backgroundColor = "#ffbade"
 
 })
+
+h1Elemento.addEventListener("mouseover", ()=>{
+    h1Elemento.style.fontSize = "68px"
+})
+
+h1Elemento.addEventListener("mouseleave", ()=>{
+    h1Elemento.style.fontSize = "34px"
+})
+
 
 //declaração de variáveis com LET e VAR e CONST
 
@@ -16,9 +27,9 @@ h1Elemento.addEventListener("click",function(){
 
 var nome = "Pedro"
 if(nome !=""){
-    var nome = "Lana"
+    let nome = "Lana"
 }
 
 //imprimindo o valor da variável
 console.log(nome)
-
+*/
