5c230aa6bb2062cd753f125b17260f3f916492c4
Petrus369 <rm97937@fiap.com.br>
2023-03-28 11:08:52 -0300

    Exemplificando evendos com Listener

--- rm97937/js/script.js
+++ rm97937/js/script.js
@@ -1,19 +1,29 @@
-//Capturando a tag com ID e colocando em uma variável var
-var h1Elemento = document.getElementById("titulo-1")
-//Adicionando um Listener de Evento
-h1Elemento.addEventListener("click", function(){
-    alert("Agora sou autônomo!")
-})
+// //Capturando a tag com ID e colocando em uma variável var
+// const h1Elemento = document.getElementById("titulo-1")
+// //Adicionando um Listener de Evento
+// h1Elemento.addEventListener("click", function(){
+//     alert("Agora sou autônomo!")
+//     h1Elemento.innerHTML = "Novo título!";
+//     h1Elemento.style.backgroundColor = "#FF0000"
+// })
 
-//Declaração de variáveis com let, var e const
+// h1Elemento.addEventListener("mouseover", ()=>{
+//     h1Elemento.style.fontSize = "68px";
+// })
 
-var nome1 = "Pedro" //O conteúdo da variável pode ser alterado durante a execução do programa
+// h1Elemento.addEventListener("mouseleave", ()=>{
+//     h1Elemento.style.fontSize = "34px";
+// })
 
-if (nome1 != ""){
-    let nome1 = "Emanuelle"
-}
+// //Declaração de variáveis com let, var e const
 
-console.log(nome1)
+// var nome1 = "Pedro" //O conteúdo da variável pode ser alterado durante a execução do programa
 
-//let nome2 = "Pedro" //O conteúdo da variável pode ser alterado durante a execução do programa
-//const nome3 = "Pedro" //O conteúdo da variável NÃO pode ser alterado durante a execução do programa
+// if (nome1 != ""){
+//     let nome1 = "Emanuelle"
+// }
+
+// console.log(nome1)
+
+// //let nome2 = "Pedro" //O conteúdo da variável pode ser alterado durante a execução do programa
+// //const nome3 = "Pedro" //O conteúdo da variável NÃO pode ser alterado durante a execução do programa
