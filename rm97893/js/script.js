447a0709294e305976cc29c71df9878d2f8e6eef
MuriloRoveri <RM97893@fiap.com.br>
2023-03-28 10:26:15 -0300

    adicionei javascript.

---
+++ rm97893/js/script.js
@@ -0,0 +1,21 @@
+// capturando a tag com id e colocando em uma varíavel var 
+var h1elemento = document.getElementById("Título-1")
+
+// adcionando um  listener de evento 
+h1elemento.addEventListener("click", function(){
+
+    alert("agora sou autonomo!")
+})
+
+//declaração de variavies com let var e const 
+
+var nome = "pedro" // podem mudar de valor ao decorrer do cod 
+let Nome = "pedro" // pode mudar de valor ao decorrer do cod 
+const noMe = "pedro " // não muda de valor  agora
+
+if(nome != ""){ 
+    let nome = enzo 
+}
+
+//nome a ser impresso
+console.log(nome)
