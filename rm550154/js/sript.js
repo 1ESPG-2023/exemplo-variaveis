df537ed8a4efc5da9d1c29803c8fd0e09634aca3
KevinBeykam <rm550154@fiap.com.br>
2023-03-28 11:48:51 -0300

    exemplificação v2

--- rm550154/js/sript.js
+++ rm550154/js/sript.js
@@ -1,24 +1,25 @@
 //Capturano a tag com ID é colocando em uma variável var 
-const h1Elemento = document.getElementById("titulo-1")
+//const h1Elemento = document.getElementById("titulo-1")
 //Adicionando um Listener de Evento
-h1Elemento.addEventListener("click",function(){
+//h1Elemento.addEventListener("click",function(){
 
-     alert("Agora sou autonômo!")
-     h1Elemento.innerHTML = "NOVO TÍTULO!";
-    h1Elemento.style.backgroundColor = "#ff0000"
-})
-h1Elemento.addEventListener("mouseover", ()=>{
-    h1Elemento.style.fontSize ="68px"
-})
+  //   alert("Agora sou autonômo!")
+  //   h1Elemento.innerHTML = "NOVO TÍTULO!";
+  //  h1Elemento.style.backgroundColor = "#ff0000"
+//})
+//h1Elemento.addEventListener("mouseover", ()=>{
+//    h1Elemento.style.fontSize ="68px"
+//})
 
 
 //Declaração de variáveis com LET e VAR e CONST
  //const nome = "Pedro"
- var nome = "AAAAAA"
+ //var nome = "AAAAAA"
  
- if(nome != ""){
-var nome = "SSSSSS"
- }
+ //if(nome != ""){
+//var nome = "SSSSSS"
+ //}
 
  //Imprimino o valor da variável
- console.log(nome)
+ //console.log(nome)
+
