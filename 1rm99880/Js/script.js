eb9f7116579e5baf3b4336d06477041f5bad12f0
GabrielMachadoCP <rm99880@fiap.com.br>
2023-03-28 11:05:07 -0300

    Adicionei eventos mouseover e mouse leave

--- 1rm99880/Js/script.js
+++ 1rm99880/Js/script.js
@@ -7,9 +7,18 @@ var h1Elemento = document.getElementById("titulo-1")
 h1Elemento.addEventListener("click", function(){
 
 alert("Agora sou autônomo!")
-
+h1Elemento.textContent = "NOVO TÍTULO";
+h1Elemento.style.backgroundColor = "#ff0000"
 })
 
+
+h1Elemento.addEventListener("mouseover", ()=>{
+    h1Elemento.style.fontSize = "68px"
+})
+
+h1Elemento.addEventListener("mouseleave", ()=>{
+    h1Elemento.style.fontSize = "34px"
+})
 //Declaração de variáveis com let var e const
 
 var nome = "Pedro"
