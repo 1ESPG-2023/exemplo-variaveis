37791580b472958883b2e026fdf5e35cf2652379
PedroZilves <rm99482@fiap.com.br>
2023-03-28 11:09:57 -0300

    Exemplos de eventos com listener

--- rm99482/js/script.js
+++ rm99482/js/script.js
@@ -1,14 +1,22 @@
-///Capturando a tag com id e colocando em uma variavel VAR
+// ///Capturando a tag com id e colocando em uma variavel VAR
 
-var h1Elemento = document.getElementById("titulo-1")
+// var h1Elemento = document.getElementById("titulo-1")
 
-//adicionando um listener/escitador de evento
+// //adicionando um listener/escitador de evento
 
-h1Elemento.addEventListener("click", function(){
-    alert("Agora sou autonomo")
-})
-//Declaração de variaveis com LET VAR e CONST
+// h1Elemento.addEventListener("click", function(){
+//     alert("Agora sou autonomo")
+//     h1Elemento.innerHTML = "NOVO TIIIIIIIIIIIITULOS!";
+//     h1Elemento.style.backgroundColor = "#00ff"
+// })
+// h1Elemento.addEventListener("mouseover", ()=>{
+// h1Elemento.style.fontSize = "69px"
+// })
+// h1Elemento.addEventListener("mouseleave",()=>{
+//    h1Elemento.style.fontSize = "34px"
+// })
+// //Declaração de variaveis com LET VAR e CONST
 
-var   Nome = "Pedro"
-let   nome = "Pedro"
-const noMe = "Pedro" // const nao pode ter seu valor alterado
+// var   Nome = "Pedro"
+// let   nome = "Pedro"
+// const noMe = "Pedro" // const nao pode ter seu valor alterado
