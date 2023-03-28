498b409a76f4462bbc72e55998c38af48303719e
emnuelle <rm97973@fiap.com.br>
2023-03-28 11:10:43 -0300

    exemplificando variaveis  kk acho

--- rm97973/js/script.js
+++ rm97973/js/script.js
@@ -1,25 +1,33 @@
-// dicas no mdn web docs
+// // dicas no mdn web docs
 
-// Capturando a tag com ID e colocando em uma variavél var
-var h1Elemento = document.getElementById("titulo-1")
+// // Capturando a tag com ID e colocando em uma variavél var
+// const h1Elemento = document.getElementById("titulo-1")
 
-// Adicionando um listener de evento 
-h1Elemento.addEventListener("click",function(){
+// // Adicionando um listener de evento 
+// h1Elemento.addEventListener("click",function(){
 
-    alert("agora sou atônomo!")
+//     alert("agora sou atônomo!")
+//     h1Elemento.innerHTML = "Novo título!";
+//     h1Elemento.style.backgroundColor = "#FB607F"
 
-})
+// })
+// h1Elemento.addEventListener("mouseover", ()=>{
+//     h1Elemento.style.fontSize="60px"
+// })
+// h1Elemento.addEventListener("mouseleave", ()=>{
+//     h1Elemento.style.fontsize="34px"
+// })
 
-// Declaração de vaiaveis como LET, VAR e CONST
+// // Declaração de vaiaveis como LET, VAR e CONST
 
-// const nome = "Pedro" //conteudo não pode ser alterado depois
+// // const nome = "Pedro" //conteudo não pode ser alterado depois
 
-var nome = "Pedro" // é considerado global
+// var nome = "Pedro" // é considerado global
 
-if(nome != ""){
-    var nome = "Emanuelle"
-}
-//Imprimindo o valor da variável
-console.log(nome)
+// if(nome != ""){
+//     var nome = "Emanuelle"
+// }
+// //Imprimindo o valor da variável
+// console.log(nome)
 
-// let nome = "Pedro"
+// // let nome = "Pedro"
