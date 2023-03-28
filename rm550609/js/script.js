e0fafb5609085b68d3ff46db98d54d9ce7dbeae3
victoriafpizza <rm550609@fiap.com.br>
2023-03-28 11:45:49 -0300

    validação ternaria

--- rm550609/js/script.js
+++ rm550609/js/script.js
@@ -29,3 +29,35 @@
 
 // //imprimindo o valor da variavel 
 // console.log(nome)
+
+// nome = "teste"
+// console.log(nome)
+// var nome
+ 
+//estrutura de decisão if  else e else if 
+//operadores logicos
+// && and
+// || OR 
+//!= NOT / DIFERENTE
+
+// let nome = null
+// console.log(nome)
+
+// if (nome != "" && nome != undefined) {
+//     console.log("a variavel esta preenchida")
+// }else if(nome==null){
+//     console.log("a variavel é null")
+// }
+// else{
+//     console.log("a variavel não esta preenchida")
+// }
+
+// operador ternario (?), veririfcar s eum numero é par ou impar
+
+let num1 = 10;
+let num2 = 5;
+
+//crie uma validação ternaria onde resulte no valro de par ou impar 
+
+let resultado = 10 % 5 ? "par" : "impar";
+console.log("o numero que esta sendo manipulado é: " + resultado)
