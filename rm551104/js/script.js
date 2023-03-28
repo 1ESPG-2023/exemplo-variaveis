a4932d63bce9c9dafd2c1494e6d54d62bc7c4f89
fernandakaory <rm551104@fiap.com.br>
2023-03-28 11:09:14 -0300

    exemplificando com listener

--- rm551104/js/script.js
+++ rm551104/js/script.js
@@ -1,25 +1,36 @@
 
-
+/*
 //capturando a tag com ID
-var h1Elemento = document.getElementById("titulo-1")
+//propriedades: texto (ex no novo tutlo)
+const h1Elemento = document.getElementById("titulo-1") //melhor usar const para declarar elemento, pois é possíevl alterar as propriedades
 
 //adicionando um listener de evento
 h1Elemento.addEventListener("click",function(){
   
   alert("Agora sou autonômo")
+  //h1Elemento.textContent="Novo título";
+  h1Elemento.innerHTML="NOVO TÍTULO";
+  h1Elemento.style.backgroundColor="#eda1bd"
+})
 
+h1Elemento.addEventListener("mouseover",()=>{
+  h1Elemento.style.fontSize="68px"
+
+}) //arrow function, simplificação da function
+h1Elemento.addEventListener("mouseleave",()=>{
+  h1Elemento.style.fontSize="34px"
 })
 
 //declaração de variáveis com let var e const
 
 //var é global
-let nome="Pedro"
+var nome="Pedro"
 if(nome!=""){
-    let nome="Fernanda"
+    var nome="Fernanda"
 }
 //imprimindo valor da variavel
 console.log(nome)
-
+*/
 
 
  
