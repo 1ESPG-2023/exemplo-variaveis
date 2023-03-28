5ac053888e52019dd8560d8d16310c5e519f7299
lucassperanzini <RM98297@fiap.com.br>
2023-03-28 11:50:30 -0300

    par ou impar condicionais

--- rm98297/js/script.js
+++ rm98297/js/script.js
@@ -1,37 +1,76 @@
-/*
-// Capturando a tag com ID e colocando em uma variável var
-const h1Elemento = document.getElementById("titulo-1")
+// /*
+// // Capturando a tag com ID e colocando em uma variável var
+// const h1Elemento = document.getElementById("titulo-1")
 
 
-//Adicionando um Listener de Evento
-h1Elemento.addEventListener("click",function(){
+// //Adicionando um Listener de Evento
+// h1Elemento.addEventListener("click",function(){
 
-    alert("Agora sou autônomo!!")
-    h1Elemento.innerHTML ="Novo título!";
-    h1Elemento.style.backgroundColor = "#F5F5"
-})
+//     alert("Agora sou autônomo!!")
+//     h1Elemento.innerHTML ="Novo título!";
+//     h1Elemento.style.backgroundColor = "#F5F5"
+// })
 
-h1Elemento.addEventListener("mouseover",()=>{
+// h1Elemento.addEventListener("mouseover",()=>{
 
-    h1Elemento.style.fontSize = "68px"
+//     h1Elemento.style.fontSize = "68px"
 
-})
+// })
 
-h1Elemento.addEventListener("mouseleave",()=>{
+// h1Elemento.addEventListener("mouseleave",()=>{
 
-    h1Elemento.style.fontSize = "34px"
+//     h1Elemento.style.fontSize = "34px"
 
-})
+// })
 
-// Declaração de variaves com LET e VAR e CONST
+// // Declaração de variaves com LET e VAR e CONST
 
 
-var nome = "Pedro"
+// var nome = "Pedro"
 
-if(nome != ""){
-    let nome = "Emenuelle"
-}
+// if(nome != ""){
+//     let nome = "Emenuelle"
+// }
 
-//imprimindo o valor de variável
-console.log(nome)
-*/
+// //imprimindo o valor de variável
+// console.log(nome)
+// */
+
+// //Estrutura de decisão If/ else / else if
+// //operadores lógicos
+// // && e AND
+// // || OR
+// //!= NOT
+
+// let nome = null
+// console.log(nome)
+
+
+// if (nome != "" && nome != undefined) {
+//     console.log("A váriavel está preenchida")
+
+// }else if(nome == null){
+//     console.log("a variável é null")
+// }
+
+
+// else {
+
+//     console.log("a variável não está preenchida")
+
+// }
+
+
+// //Operador ternário
+
+let num1 = 10;
+let num2 = 5
+
+//Crie uma validação pe=ternária em que resulte no valor de par ou ímpar
+
+let resultado = !(10 % 5) ?  "PAR" : "ÍMPAR" ;
+
+    console.log("O número que está sendo manipulado é : " + resultado)
+    
+
+
