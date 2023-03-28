d5791dd69c9f339e2874fa368dc6d8b681cc2630
M4thz12 <mr98832@fiap.com.br>
2023-03-28 11:18:30 -0300

    commit 28/03

--- rm98832/js/script.js
+++ rm98832/js/script.js
@@ -1,24 +1,24 @@
-//iniciando pasta em JavaScript
-//capturando a tag com ID e colocando em uma variavel var
-var h1Elemento = document.getElementById("titulo-1")
-//adicionando evento de listener
-h1Elemento.addEventListener("click",function(){
-    h1Elemento.innerHTML = "Novo Titulo"
-    h1Elemento.style.backgroundColor = "#ff0000"
-})
-h1Elemento.addEventListener("mouseleave", ()=>{
-    h1Elemento.style.fontSize = "68px"
-})
-h1Elemento.addEventListener("mouseover", ()=>{
-    h1Elemento.style.fontSize = "32px"
-})
-//declaração de variaveis com var let e const
-let name = "Pedro"
+// //iniciando pasta em JavaScript
+// //capturando a tag com ID e colocando em uma variavel var
+// var h1Elemento = document.getElementById("titulo-1")
+// //adicionando evento de listener
+// h1Elemento.addEventListener("click",function(){
+//     h1Elemento.innerHTML = "Novo Titulo"
+//     h1Elemento.style.backgroundColor = "#ff0000"
+// })
+// h1Elemento.addEventListener("mouseleave", ()=>{
+//     h1Elemento.style.fontSize = "68px"
+// })
+// h1Elemento.addEventListener("mouseover", ()=>{
+//     h1Elemento.style.fontSize = "32px"
+// })
+// //declaração de variaveis com var let e const
+// let name = "Pedro"
  
-if(name != ""){
-    let name = "rodrigo"
-    console.log(name)
-}
- console.log(name)
+// if(name != ""){
+//     let name = "rodrigo"
+//     console.log(name)
+// }
+//  console.log(name)
 
 
