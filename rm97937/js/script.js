19226143cf298487b1fba1e431ee17779a575d49
Petrus369 <rm97937@fiap.com.br>
2023-03-28 11:48:48 -0300

    Usando operadores lógicos e operador ternário

--- rm97937/js/script.js
+++ rm97937/js/script.js
@@ -27,3 +27,32 @@
 
 // //let nome2 = "Pedro" //O conteúdo da variável pode ser alterado durante a execução do programa
 // //const nome3 = "Pedro" //O conteúdo da variável NÃO pode ser alterado durante a execução do programa
+
+//Estrutura de decisão if - else - else if
+//Operadores lógicos: && => and, || => or, ! => not, != => not equal
+
+let nome = null
+console.log(nome)
+if(nome != "" && nome != undefined) {
+    console.log("A variável está preenchida")
+}
+
+else if(nome == null) {
+    console.log("A variável é null")
+}
+
+else {
+    console.log("A variável não está preenchida")
+}
+
+//Operadores relacionais são os mesmos que os do python.
+
+//Operador ternário: (?)
+
+let num1 = 10;
+let num2 = 5;
+
+//Criando uma validação ternária onde resulte no valor de PAR ou IMPAR:
+
+let resultado = !(10%5) ? "Par" : "Impar";
+console.log("O número que está sendo manipulado é " + resultado)
