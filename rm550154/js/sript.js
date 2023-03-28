8d6596e4035eb6fefdf9a17064114d23293c31e1
KevinBeykam <rm550154@fiap.com.br>
2023-03-28 11:17:35 -0300

    exemplificando variaveis

--- rm550154/js/sript.js
+++ rm550154/js/sript.js
@@ -1,20 +1,23 @@
-
-
 //Capturano a tag com ID é colocando em uma variável var 
-var h1Elemento = document.getElementById("titulo-1")
+const h1Elemento = document.getElementById("titulo-1")
 //Adicionando um Listener de Evento
 h1Elemento.addEventListener("click",function(){
 
-    alert("Agora sou autonômo")
-
+     alert("Agora sou autonômo!")
+     h1Elemento.innerHTML = "NOVO TÍTULO!";
+    h1Elemento.style.backgroundColor = "#ff0000"
 })
+h1Elemento.addEventListener("mouseover", ()=>{
+    h1Elemento.style.fontSize ="68px"
+})
+
 
 //Declaração de variáveis com LET e VAR e CONST
  //const nome = "Pedro"
- var nome = "Pedro"
+ var nome = "AAAAAA"
  
  if(nome != ""){
-    var nome = "Emanuelle"
+var nome = "SSSSSS"
  }
 
  //Imprimino o valor da variável
