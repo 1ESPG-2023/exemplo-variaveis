8a3dc515d8e39a699cab58f0c46fac2562311563
EnzoMShiotuqui <RM551508@fiap.com.br>
2023-03-28 11:08:30 -0300

    Mudando o arquivo JS

--- rm551508/js/script.js
+++ rm551508/js/script.js
@@ -1,21 +1,30 @@
 // capturando a tag com id e colocando em uma varíavel var 
-var h1elemento = document.getElementById("Título-1")
+ /*const h1elemento = document.getElementById("Título-1")
 
 // adcionando um  listener de evento 
 h1elemento.addEventListener("click", function(){
 
     alert("agora sou autonomo!")
+    h1elemento.innerHTML = "Novo título"
+    h1elemento.style.backgroundColor = "#ff0000"
 })
 
+h1elemento.addEventListener("mouseover", function(){
+    h1elemento.style.fontSize = "68px"
+})
+
+h1elemento.addEventListener("mouseleave",function(){
+    h1elemento.style.fontSize = "32px"
+})
 //declaração de variavies com let var e const 
 
 var nome = "pedro" // podem mudar de valor ao decorrer do cod 
 let Nome = "pedro" // pode mudar de valor ao decorrer do cod 
-const noMe = "pedro " // não muda de valor  agora
+const noMe = "pedro " // não muda de valor 
 
 if(nome != ""){ 
     let nome = enzo 
 }
 
 //nome a ser impresso
-console.log(nome)
+console.log(nome) */
