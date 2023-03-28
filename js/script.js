292753eaabe4393707113ef4c52d09ac202b0350
MachadONLY <rm551570@fiap.com.br>
2023-03-28 11:09:37 -0300

    exemplificando eventos com listener

--- js/script.js
+++ js/script.js
@@ -7,18 +7,30 @@ var h1Elemento = document.getElementById("Titulo-1")
 h1Elemento.addEventListener("click",function(){
 
     alert("Agora sou autonômo")
+    h1Elemento.innerHTML = "Novo Título"; // (.textContent) após o click e alert ele altera o título
+    h1Elemento.style.backgroundColor = "#FF0000
 })
 
+h1Elemento.addEventListener("mouseover", ()=>(
+    h1Elemento.style.fontSize = "68px"
+
+))
+
+h1Elemento.addEventListener("mouseleave", ()=>(
+    h1Elemento.style.fontSize = "34px"
+))
 //Declaração de Variaveis com LET e VAR e CONST
 
 //const nome = "Pedro"
 
+
 var nome = "Pedro"
 
-if(nome != ""){}
-    var nome = "Lucas"
+if(nome != ""){
+    let nome = "Lucas"
+}
 
 
-
-
+//imprimindo o valor da variável
+console.log(nome)
 
