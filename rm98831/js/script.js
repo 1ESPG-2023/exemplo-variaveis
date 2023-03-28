229e5920e0674332e3c72f797538dc9cfdbe2171
Henriquedbrito <rm98831@fiap.com.br>
2023-03-28 11:09:06 -0300

    exemplificando eventos com listener

--- rm98831/js/script.js
+++ rm98831/js/script.js
@@ -1,15 +1,24 @@
 
 
 //Capturando a tag com ID e colocando em uma variável var
-var h1Elemento = document.getElementById("titulo-1")
+const h1Elemento = document.getElementById("titulo-1")
 
 //Adicionando um Listener de Evento
 h1Elemento.addEventListener("click", function(){
 
     alert("Agora sou autonônomo!")
+    h1Elemento.innerHTML = "NOVO TÍTULO!";
+    h1Elemento.style.backgroundColor = "#ff0000"
 
 })
 
+h1Elemento.addEventListener("mouseover", ()=>{
+    h1Elemento.style.fontSize = "68px"
+})
+
+h1Elemento.addEventListener("mouseleave", ()=>{
+    h1Elemento.style.fontSize = "34px"
+})
 //Declaração de variáveis com LET e VAR e CONST
 
 var nome = "Pedro"
