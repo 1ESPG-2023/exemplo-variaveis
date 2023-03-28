433342342f7cf1bd759ca5a48f481b534c7f1c3e
geovannaacunha <rm97736@fiap.com.br>
2023-03-28 11:08:47 -0300

    Adiconando eventos: mudando titulos cor eInteração

--- RM97736/js/script.js
+++ RM97736/js/script.js
@@ -6,6 +6,16 @@ var h1Elemento = document.getElementById("título-1")
 // Adicionando um listener de evento 
 h1Elemento.addEventListener("click", function(){
     alert("Agora sou autônomo")
+    h1Elemento.innerHTML = "NOVO TÍTULO"
+    h1Elemento.style.backgroundColor = "#ff0000"
+})
+
+h1Elemento.addEventListener("mouseover", ()=>{
+    h1Elemento.style.fontSize = "68px"
+})
+
+h1Elemento.addEventListener("mouseleave", ()=>{
+    h1Elemento.style.fontSize = "34px"
 })
 
 // Declaração de variaveis com LET e VAR e CONST
