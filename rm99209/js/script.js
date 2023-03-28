dfde0e34aad7e2e6c65a3d654f49ab657cbff9db
BernardoPRocha <RM99209@fiap.com>
2023-03-28 11:50:11 -0300

    exemplos de variaveis

--- rm99209/js/script.js
+++ rm99209/js/script.js
@@ -1,23 +1,52 @@
+ //Capturando a tag com ID e colocando em uma variável var
+ /* var h1Elemento = document.getElementById("titulo-1")
 
+ //Adicionando um Listener de Evento
+ h1Elemento.addEventListener("click",function(){
+     //alert("Agora sou autonômo!")
+     //h1Elemento.innerHTML = "NOVO TÍTULO!";
+     //h1Elemento.style.backgroundColor = "#ff0000"
+ })
 
-//Capturando a tag com ID e colocando em uma variável var 
-var h1Elemento = document.getElementById("título-1")
+ h1Elemento.addEventListener("mouseover", ()=>{
+    h1Elemento.style.fontSize = "68px"
+ })
 
-//adicionando um listener de Evento
-h1Elemento.addEventListener("click", function(){
-    alert("Título 1") 
-})
+ h1Elemento.addEventListener("mouseleave", ()=>{
+    h1Elemento.style.fontSize = "34px"
+ })
 
-//Declaração de variáveis com LET, VAR e CONST 
+ //Declaração de variáveis com LET e VAR e CONST
 
-//const noMe = "Pedro"
-var nome = "Pedro"
+ const nome = "Pedro"
 
-if(nome != ""){
-    var nome = "Bernardo"
-}
+ var nome = "Pedro"
 
-console.log(nome)
+ if(nome != ""){
+    var nome = "Emanuelle"
+ } */
 
+ //Imprimindo o valor da variável
+ //console.log(nome)
 
-    
+ let nome = null
+  
+ if(nome != "" && nome != undefined) {
+    console.log("A variavel está preenchida")
+ } else if(nome == null) {
+    console.log("A variavel é null")
+ }
+ else {
+    console.log("A variavel não está preenchida")
+ }
+
+ //OPERADOR TERNÁRIO (?)
+
+ let num1 = 10;
+ let num2 = 5;
+
+ //crie uma validação ternaria onde resulte no valor de par ou impar
+
+ let resultado = !(10%5) ?  "PAR" : "IMPAR";
+
+ console.log("O número que está sendo manipulado é :" + resultado)
