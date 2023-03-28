1ed4dc0ccf4f24fa0c0fbdeec9b407f86d322f35
RicardoHilknerNeto <rm550757@fiap.com.br>
2023-03-28 11:13:07 -0300

    Exemplificando eventos com Listener

--- rm550757/js/script.js
+++ rm550757/js/script.js
@@ -1,36 +1,36 @@
 
 
-//Capturando a tag com ID e colocando em uma variável var
-const h1Element = document.getElementById("titulo-1")
+// //Capturando a tag com ID e colocando em uma variável var
+// const h1Element = document.getElementById("titulo-1")
 
-//Adicionando um Listener de Evento
-h1Element.addEventListener("click",function(){
+// //Adicionando um Listener de Evento
+// h1Element.addEventListener("click",function(){
 
-    alert("Agora sou autonômo")    
-    h1Element.textContent = "Novo título";
-    h1Element.style.backgroundColor = "#ff0000";
+//     alert("Agora sou autonômo")    
+//     h1Element.textContent = "Novo título";
+//     h1Element.style.backgroundColor = "#ff0000";
 
-})
+// })
 
-h1Element.addEventListener("mouseover", ()=>{
-    h1Element.style.fontSize = "48px"
-})
+// h1Element.addEventListener("mouseover", ()=>{
+//     h1Element.style.fontSize = "48px"
+// })
 
-h1Element.addEventListener("mouseleave", ()=>{
-    h1Element.style.fontSize = "34px"
-})
-
-
-//Declaração de variaveos com LET, VAR e CONST
-
-var name = "Ricardo" 
-if(nome != ""){
-    var nome = "Neto"
-}
-
-console.log(nome)
+// h1Element.addEventListener("mouseleave", ()=>{
+//     h1Element.style.fontSize = "34px"
+// })
 
 
+// //Declaração de variaveos com LET, VAR e CONST
 
-//let name = "Ricardo"
-//const name = "Ricardo"
+// var name = "Ricardo" 
+// if(nome != ""){
+//     var nome = "Neto"
+// }
+
+// console.log(nome)
+
+
+
+// //let name = "Ricardo"
+// //const name = "Ricardo"
