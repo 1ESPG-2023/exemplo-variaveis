05b92569fa9b36fdb6e5e03585185228f8ed6fd3
MuriloRoveri <RM97893@fiap.com.br>
2023-03-28 11:14:35 -0300

    js adicionado como comentario

--- rm97893/js/script.js
+++ rm97893/js/script.js
@@ -1,31 +1,31 @@
-/* // capturando a tag com id e colocando em uma varíavel var 
-const h1elemento = document.getElementById("Título-1")
+// // capturando a tag com id e colocando em uma varíavel var 
+// const h1elemento = document.getElementById("Título-1")
 
-// adcionando um  listener de evento 
-h1elemento.addEventListener("click", function(){
+// // adcionando um  listener de evento 
+// h1elemento.addEventListener("click", function(){
 
-    alert("agora sou autonomo!")
-    h1elemento.innerHTML = "NOVO TITULO"
-    h1elemento.style.backgroundColor = "#ff0000"
-})
+//     alert("agora sou autonomo!")
+//     h1elemento.innerHTML = "NOVO TITULO"
+//     h1elemento.style.backgroundColor = "#ff0000"
+// })
 
-h1elemento.addEventListener("mouseover", ()=>{
-    h1elemento.style.fontSize = "68px"
-})
+// h1elemento.addEventListener("mouseover", ()=>{
+//     h1elemento.style.fontSize = "68px"
+// })
 
-h1elemento.addEventListener("mouseleave", ()=>{
-    h1elemento.style.fontSize = "34px"
-})
+// h1elemento.addEventListener("mouseleave", ()=>{
+//     h1elemento.style.fontSize = "34px"
+// })
 
-//declaração de variavies com let var e const 
+// //declaração de variavies com let var e const 
 
-var nome = "pedro" // podem mudar de valor ao decorrer do cod 
-let Nome = "pedro" // pode mudar de valor ao decorrer do cod 
-const noMe = "pedro " // não muda de valor  agora
+// var nome = "pedro" // podem mudar de valor ao decorrer do cod 
+// let Nome = "pedro" // pode mudar de valor ao decorrer do cod 
+// const noMe = "pedro " // não muda de valor  agora
 
-if(nome != ""){ 
-    var nome = murilo
-}
+// if(nome != ""){ 
+//     var nome = murilo
+// }
 
-//nome a ser impresso
-console.log(nome) */
+// //nome a ser impresso
+// console.log(nome)
