50c412a4ade46bc518208e989646f3604b7b3ef0
Rodrigoou <rm550816@fiap.com.br>
2023-03-28 11:10:18 -0300

    comitando

--- rm550816/js/script.js
+++ rm550816/js/script.js
@@ -1,21 +1,30 @@
 
 
 //Capturando a tag com ID e colocando em uma variável var
-var h1Elemento = document.getElementById("titulo-1")
+//var h1Elemento = document.getElementById("titulo-1")
 
 //Adicionando um Listener de Evento
-h1Elemento.addEventListener("click", function(){
+//h1Elemento.addEventListener("click", function(){
 
 
-alert("Agora sou autonomo")
+//   alert("Agora sou autonomo")
+//   h1Elemento.innerHTML="NOVIDADES";
+//    h1Elemento.style.backgroundColor = "#ff0000"
 
 
-})
+//})
+
+//h1Elemento.addEventListener("mouseover", ()=>{
+//    h1Elemento.style.fontSize = "68px"
+//})
+//h1Elemento.addEventListener("mouseleave", ()=>{
+//    h1Elemento.style.fontSize = "32px"
+//})
 
 //Declaração de variáveis com LET e VAR e CONST
-var nome = "Pedro"
+//var nome = "Pedro"
 
-if(nome != ""){
-    var name ="rodrigo"
-}
-console.log(name)
+//if(nome != ""){
+//    var name ="rodrigo"
+//}
+//console.log(name)
