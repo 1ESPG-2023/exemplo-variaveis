914ba290d3fd81e71e430024c09228d833760bca
lucassperanzini <RM98297@fiap.com.br>
2023-03-28 11:11:39 -0300

    exemplos eventos listener

--- rm98297/js/script.js
+++ rm98297/js/script.js
@@ -1,14 +1,27 @@
-
+/*
 // Capturando a tag com ID e colocando em uma variável var
-var h1Elemento = document.getElementById("titulo-1")
+const h1Elemento = document.getElementById("titulo-1")
 
 
 //Adicionando um Listener de Evento
 h1Elemento.addEventListener("click",function(){
 
     alert("Agora sou autônomo!!")
+    h1Elemento.innerHTML ="Novo título!";
+    h1Elemento.style.backgroundColor = "#F5F5"
 })
 
+h1Elemento.addEventListener("mouseover",()=>{
+
+    h1Elemento.style.fontSize = "68px"
+
+})
+
+h1Elemento.addEventListener("mouseleave",()=>{
+
+    h1Elemento.style.fontSize = "34px"
+
+})
 
 // Declaração de variaves com LET e VAR e CONST
 
@@ -21,3 +34,4 @@ if(nome != ""){
 
 //imprimindo o valor de variável
 console.log(nome)
+*/
