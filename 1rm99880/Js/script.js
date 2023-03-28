ff60a1581cb9d6462de6af659187ceb7d7056ff1
GabrielMachadoCP <rm99880@fiap.com.br>
2023-03-28 11:09:01 -0300

    Exemplificando eventos com listener

--- 1rm99880/Js/script.js
+++ 1rm99880/Js/script.js
@@ -1,31 +1,31 @@
 
 
-//Capturando a tag com o ID e colocando em uma variável var
-var h1Elemento = document.getElementById("titulo-1")
+// //Capturando a tag com o ID e colocando em uma variável var
+// var h1Elemento = document.getElementById("titulo-1")
 
-//Adicionando um listener de evento
-h1Elemento.addEventListener("click", function(){
+// //Adicionando um listener de evento
+// h1Elemento.addEventListener("click", function(){
 
-alert("Agora sou autônomo!")
-h1Elemento.textContent = "NOVO TÍTULO";
-h1Elemento.style.backgroundColor = "#ff0000"
-})
+// alert("Agora sou autônomo!")
+// h1Elemento.textContent = "NOVO TÍTULO";
+// h1Elemento.style.backgroundColor = "#ff0000"
+// })
 
 
-h1Elemento.addEventListener("mouseover", ()=>{
-    h1Elemento.style.fontSize = "68px"
-})
+// h1Elemento.addEventListener("mouseover", ()=>{
+//     h1Elemento.style.fontSize = "68px"
+// })
 
-h1Elemento.addEventListener("mouseleave", ()=>{
-    h1Elemento.style.fontSize = "34px"
-})
-//Declaração de variáveis com let var e const
+// h1Elemento.addEventListener("mouseleave", ()=>{
+//     h1Elemento.style.fontSize = "34px"
+// })
+// //Declaração de variáveis com let var e const
 
-var nome = "Pedro"
+// var nome = "Pedro"
 
-if(nome != ""){
-    var nome = "Emanuelle"
-}
+// if(nome != ""){
+//     var nome = "Emanuelle"
+// }
 
-//Imprimindo o nome para ser impresso
-console.log(nome)
+// //Imprimindo o nome para ser impresso
+// console.log(nome)
