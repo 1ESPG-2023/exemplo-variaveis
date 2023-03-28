c9558b462d8d1696e04807bcff1fe81d23f48580
MatheusCosta616 <rm>
2023-03-28 11:15:50 -0300

    atualização1

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
 //capturando o ID e colocando em uma var
 
-let h1Elemento = document.getElementById("titulo-1")
+const h1Elemento = document.getElementById("titulo-1")
 
-h1Elemento = addEventListener("click", function(){
-    alert("Agora sou autônomo")
+//h1Elemento = addEventListener("click", function(){
+ //   alert("Agora sou autônomo");
+ //   h1Elemento.innerHTML = "NOVO TÍTULO!"
+  //  h1Elemento.style.backgroundColor = "red"
+//})
+
+h1Elemento.addEventListener("mouseover", () =>{
+    h1Elemento.style.fontSize = "68px"
+})
+
+
+h1Elemento.addEventListener("mouseleave", () =>{
+    h1Elemento.style.fontSize = "34px"
 })
 
 var nome = "Pedro"
