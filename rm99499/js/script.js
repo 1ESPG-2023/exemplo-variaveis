8e1c0368f464f66738be3e948769c47206001b52
GuiEspimFiap <rm99499@fiap.com.br>
2023-03-28 11:16:37 -0300

    js

--- rm99499/js/script.js
+++ rm99499/js/script.js
@@ -1,4 +1,4 @@
-// capiturando a tag por id e colocando em uma variável var
+/* // capiturando a tag por id e colocando em uma variável var
 
 var h1Element = document.getElementById("title-1"); 
 
@@ -16,4 +16,20 @@ if (nome !== "") {
 }
 console.log(nome)
 
-// const nome = "Guilherme"; 
+// const nome = "Guilherme";  */
+
+const h1Element = document.querySelector("h1");
+
+h1Element.addEventListener("click", function () {
+
+    alert("agora sou atônomo!")
+    h1Element.innerHTML = "Novo título!";
+    h1Element.style.backgroundColor = "#FB607F"
+
+})
+h1Element.addEventListener("mouseover", () => {
+    h1Element.style.fontSize = "60px"
+})
+h1Element.addEventListener("mouseleave", () => {
+    h1Element.style.fontsize = "34px"
+})
