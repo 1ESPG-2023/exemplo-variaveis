55dc70ebae8e30657f42e3bd67b5b1dae23f8a47
RicardoHilknerNeto <rm550757@fiap.com.br>
2023-03-28 11:05:32 -0300

    Adicionando um evento com o mouse

--- rm550757/js/script.js
+++ rm550757/js/script.js
@@ -8,8 +8,18 @@ h1Element.addEventListener("click",function(){
 
     alert("Agora sou autonômo")    
     h1Element.textContent = "Novo título";
+    h1Element.style.backgroundColor = "#ff0000";
 
 })
+
+h1Element.addEventListener("mouseover", ()=>{
+    h1Element.style.fontSize = "48px"
+})
+
+h1Element.addEventListener("mouseleave", ()=>{
+    h1Element.style.fontSize = "34px"
+})
+
 
 //Declaração de variaveos com LET, VAR e CONST
 
