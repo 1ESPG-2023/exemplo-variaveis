d62d6b495c7c0d6326c7c773b021232a5534a0ff
MuriloRoveri <RM97893@fiap.com.br>
2023-03-28 11:09:09 -0300

    Exemplificando eventos com o Listener.

--- rm97893/js/script.js
+++ rm97893/js/script.js
@@ -1,10 +1,20 @@
-// capturando a tag com id e colocando em uma varíavel var 
-var h1elemento = document.getElementById("Título-1")
+/* // capturando a tag com id e colocando em uma varíavel var 
+const h1elemento = document.getElementById("Título-1")
 
 // adcionando um  listener de evento 
 h1elemento.addEventListener("click", function(){
 
     alert("agora sou autonomo!")
+    h1elemento.innerHTML = "NOVO TITULO"
+    h1elemento.style.backgroundColor = "#ff0000"
+})
+
+h1elemento.addEventListener("mouseover", ()=>{
+    h1elemento.style.fontSize = "68px"
+})
+
+h1elemento.addEventListener("mouseleave", ()=>{
+    h1elemento.style.fontSize = "34px"
 })
 
 //declaração de variavies com let var e const 
@@ -14,8 +24,8 @@ let Nome = "pedro" // pode mudar de valor ao decorrer do cod
 const noMe = "pedro " // não muda de valor  agora
 
 if(nome != ""){ 
-    let nome = murilo
+    var nome = murilo
 }
 
 //nome a ser impresso
-console.log(nome)
+console.log(nome) */
