06b85d2f89c68f0a75114f4468e216c402f024cf
M4thz12 <mr98832@fiap.com.br>
2023-03-28 11:16:57 -0300

    commit 28/03

--- rm98832/js/script.js
+++ rm98832/js/script.js
@@ -3,12 +3,22 @@
 var h1Elemento = document.getElementById("titulo-1")
 //adicionando evento de listener
 h1Elemento.addEventListener("click",function(){
-    alert("agora sou autonomo")
+    h1Elemento.innerHTML = "Novo Titulo"
+    h1Elemento.style.backgroundColor = "#ff0000"
+})
+h1Elemento.addEventListener("mouseleave", ()=>{
+    h1Elemento.style.fontSize = "68px"
+})
+h1Elemento.addEventListener("mouseover", ()=>{
+    h1Elemento.style.fontSize = "32px"
 })
 //declaração de variaveis com var let e const
-var name = "Pedro"
+let name = "Pedro"
  
 if(name != ""){
-    var name = "rodrigo"
+    let name = "rodrigo"
+    console.log(name)
 }
  console.log(name)
+
+
