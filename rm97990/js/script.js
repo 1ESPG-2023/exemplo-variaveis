f25b0631242c4e794cdcbb5784d18cec36bf8d91
MirellaRossi <rm97990@fiap.com.br>
2023-03-28 11:07:50 -0300

    adicionad funções ao script para h1elemento

--- rm97990/js/script.js
+++ rm97990/js/script.js
@@ -1,20 +1,31 @@
-
+/*
 
 //Capturando a tag com id e colocando em uma variável var
-var h1Elemento = document.getElementById("titulo 1")
+const h1Elemento = document.getElementById("titulo 1")
 //adicionando um listener de eventos
 h1Elemento.addEventListener("click",function(){
     alert("Agora sou autonômo")
+    h1Elemento.textContent=("Novo Título!"); //adc um novo titulo após o evento de click e alert
+    h1Elemento.innerHTML="Novo Título!"
+    h1Elemento.style.backgroundColor ="#ff0000"
 })
 
+h1Elemento.addEventListener("mouseover", ()=>{
+    h1Elemento.style.fontSize = "68px" //mudança de taamnho da fonte quando passa o mouse
+
+})
+h1Elemento.addEventListener("mouseleave", ()=>{
+h1Elemento.style.fontSize="34px"//mudaça da fonte quao tiramos o mouse
+
+})
 //declaração de variáveis com Let e var e const
 //const nome ="Mirella"
 
 var nome = "Mirella"
  if(nome != ""){
-    let nome = "Pedro"
+    var nome = "Pedro"
 
  }
  console.log(nome)
 
- 
+ */
