e3cbe16b2c5610d611b942488ac8ef96d1950503
LanaAuada <rm551143@fiap.com.br>
2023-03-28 11:48:39 -0300

    operadores

--- rm551143/js/script.js
+++ rm551143/js/script.js
@@ -33,3 +33,35 @@ if(nome !=""){
 //imprimindo o valor da variável
 console.log(nome)
 */
+
+//estrutura de decisão if / else / else if 
+//operadores lógicos 
+//&& AND
+//|| OR
+// != NOT / DIFERENTE
+
+/*let nome = null
+console.log(nome)
+
+if(nome!="" && nome !=undefined) {
+    console.log("A variável está preenchida")
+}else if(nome==null){
+    console.log("A variável é null")
+}
+else{
+console.log("A variável não está preenchida")
+}
+*/
+
+//operadores relacionais igual do python 
+
+//operador ternário
+
+let num1 = 10;
+let num2 = 5;
+
+//crie uma validação ternária onde resulte no valor de PAR ou ÍMPAR
+//let resultado = 10%5 ?"PAR" : "ÍMPAR";
+// lê como positivo logo se resultado é 0 dá ímpar(false)
+let resultado = !(10%5) ?"PAR" : "ÍMPAR";
+console.log("O número que esá sendo manipulado é:" + resultado)
