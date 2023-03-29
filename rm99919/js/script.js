e7d255c4e3e43a0e6fdd3d04df2f44c1da6c717e
FernandoPLP <rm99919@fiap.com.br>
2023-03-29 15:42:45 -0300

    Estruturas de decisão e operador ternario

--- rm99919/js/script.js
+++ rm99919/js/script.js
@@ -1,16 +1,78 @@
-//capturando a tag com id e colocando em um variavel var
-var h1Elemento = document.getElementById("titulo-1")
+// const h1Elemento = document.getElementById("titulo-1") 
 
-//adicionando um listener evenet
-h1Elemento.addEventListener("click", function(){
-    alert("Alerta!" )
-})
+//adicionando um listener evenet 
+// h1Elemento.addEventListener("click", function(){ 
+//     alert("Alerta!" );vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv 
+//     h1Elemento.innerHTML = "Novo titulo";vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv 
+//     h1Elemento.style.backgroundColor = "#ff0000" 
+// }) 
 
-var nome = "Fernando"
-//let nome1 = "Fernando"
-//const nome2 = "Fernando"
-if(nome != ""){
-    var nome="Pedro"
-}
+// h1Elemento.addEventListener("mouseover", function(){ 
+//     h1Elemento.style.fontSize = "58px" 
+// }) 
 
-console.log(nome)
+  
+
+// h1Elemento.addEventListener("mouseleave", function(){ 
+//     h1Elemento.style.fontSize = "34px" 
+// }) 
+
+
+// var nome = "Fernando" 
+//let nome1 = "Fernando" 
+//const nome2 = "Fernando" 
+// if(nome != ""){ 
+//     var nome="Pedro" 
+//     let nome1="Pedro" 
+//     const nome2="Pedro" 
+//     console.log(nome) 
+//     console.log(nome1) 
+//     console.log(nome2) 
+// } 
+
+  
+
+// console.log(nome) 
+// console.log(nome1) 
+// console.log(nome2) 
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////// 
+
+//estrutura de decisão if / else / elif 
+//operadores lógicos  
+// &&and  
+// != no 
+// ||or 
+
+  
+
+// let nome =null 
+// if (nome != "" && nome!= undefined ) { 
+//     console.log("A variavel está preenchida") 
+// }else if(nome == null){ 
+//     console.log("A variavel é null") 
+// } 
+// else{ 
+//     console.log("A variavel está vazia") 
+// } 
+
+  
+
+/* 
+OPERADORES RELACIONAIS 
+> MAIOR 
+< MENOR 
+>= MAIOR OU IGUAL 
+<= MENOR OU IGUAL 
+!= DIFERENTE 
+== IGUAL 
+*/ 
+
+  
+
+/*OPERADOR TERNARIO*/ 
+let num= 10 ; 
+let num2 = 5;   
+//crie uma validacao ternario onde resulte no valor de par ou impar 
+let resultado = !(10%5) ? "Par" : "Impar" 
+console.log("O numero que está sendo manipulado é: " + resultado) 
