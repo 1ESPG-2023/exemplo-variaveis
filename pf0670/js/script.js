6934e871994bacf6771acf2293ceba0cddc14abb
anazerlin <rm98065@fiap.com>
2023-04-04 11:00:29 -0300

    declaração de arrys e getelementbytagname

--- pf0670/js/script.js
+++ pf0670/js/script.js
@@ -1,7 +1,21 @@
 
 //declarando um array 
-let nomes = ["nome1", "nome2", "nome3"]
-console.table(nomes)
+// let nomes = ["nome1", "nome2", "nome3"]
+// console.table(nomes)
 
-//imprimir apenas 1 item do array
-console.log(nomes[2])
+// //imprimir apenas 1 item do array
+// console.log(nomes[2])
+
+
+//recuperando elementos de listas atraves da função
+//getElementByTagName
+//const itemsDeLista = document.getElementsByTagName("li")
+
+// let num1 = [10, 20, 30]
+// let num2 = [40, 50, 60, 70, 80]
+// let num3 
+
+// num3 = [num1, num2]
+
+// //apresentando interpolação junto com arrays
+// console.log("valor do array 3" + num3)
