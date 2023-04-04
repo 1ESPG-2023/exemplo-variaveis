3293ec88bca3f70307485c2af9b7304dcf298516
hellen-dev <rm98284@fiap.com.br>
2023-04-04 11:00:34 -0300

    declared arrays and getElementByTagName

--- pf0670/js/script.js
+++ pf0670/js/script.js
@@ -20,3 +20,18 @@ if(nome != "") {
 
 //printing the variable value
 console.log(nome)
+
+
+// recuperando elementos de lista através da função
+// getElementByTagName
+
+// const itensDeLista = document.getElementsByTagName("li")
+
+// let num1 = [10, 20, 30]
+// let num2 = [40, 50, 60, 70, 80]
+// let num3
+
+// num3 = [num1, num2]
+// //presenting 'interpolação' w/ arrays
+// console.log("Valor do array 3: " + num3)
+
