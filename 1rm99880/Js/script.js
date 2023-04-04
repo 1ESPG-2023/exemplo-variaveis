c0f585e722f63c6ce009677eb1c6448788adcc2b
GabrielMachadCP <rm99880@fiap.com.br>
2023-04-04 10:24:35 -0300

    Começando uma nova aula 4 de abril

--- 1rm99880/Js/script.js
+++ 1rm99880/Js/script.js
@@ -1,78 +1,9 @@
-
-
-// //Capturando a tag com o ID e colocando em uma variável var
-// var h1Elemento = document.getElementById("titulo-1")
-
-// //Adicionando um listener de evento
-// h1Elemento.addEventListener("click", function(){
-
-// alert("Agora sou autônomo!")
-// h1Elemento.textContent = "NOVO TÍTULO";
-// h1Elemento.style.backgroundColor = "#ff0000"
-// })
-
-
-// h1Elemento.addEventListener("mouseover", ()=>{
-//     h1Elemento.style.fontSize = "68px"
-// })
-
-// h1Elemento.addEventListener("mouseleave", ()=>{
-//     h1Elemento.style.fontSize = "34px"
-// })
-// //Declaração de variáveis com let var e const
-
-// var nome = "Pedro"
-
-// if(nome != ""){
-//     var nome = "Emanuelle"
-// }
-
-// //Imprimindo o nome para ser impresso
-// console.log(nome)
-
-
-
-
-//Estrutura de decisão IF / else / else if
-//Operadores lógicos
-//&& AND
-//|| OR
-// ! NOT
-// !=  NOT / DIFERENTE
-
-// let nome = null
-// console.log(nome)
-
-// if (nome != "" && nome != undefined){
-//     console.log("A variável está preenchida!")
-// }
-// else if(nome == null){
-//     console.log("A variável é null")
-// }
-// else{
-//     console.log("A variável não está preenchida!")
-//     }
-
-
-
-
-
-//Operadores Relacionais
-// > Maior
-// < Menor
-// >= Maior ou igual
-// <= Menor ou igual
-
-
-
-
-
-
 //Operador Ternário ( ? )
 
-let num1 = 10;
-let num2 = 5;
-//crie uma validação ternária onde resulte no valor de PAR ou ÍMPAR!
+// let num1 = 10;
+// let num2 = 5;
+// //crie uma validação ternária onde resulte no valor de PAR ou ÍMPAR!
 
-let resultado = !(10%5) ? "PAR" : "ÍMPAR";
-console.log("O número que está sendo manipulado é: "+ resultado)
+// let resultado = !(10%5) ? "PAR" : "ÍMPAR";
+// console.log("O número que está sendo manipulado é: "+ resultado)
+
