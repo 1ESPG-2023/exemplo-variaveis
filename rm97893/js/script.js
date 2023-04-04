92740b53720617282fea18495e213fc0d750f9de
MuriloRoveri <RM97893@fiap.com.br>
2023-04-04 11:00:43 -0300

    Declaração arrays + getElementByTagName.

--- rm97893/js/script.js
+++ rm97893/js/script.js
@@ -52,10 +52,31 @@
 
 // Operador Ternário ( ? )
 
-let num1 = 10;
-let num2 = 5;
+// let num1 = 10;
+// let num2 = 5;
 
 // Crie uma validação ternária onde resulte no valor de PAR ou ÍMPAR!
 
-let resultado = 10%5 == 0 ? "PAR" : "ÍMPAR"
-console.log("O número que está sendo manipulado é :" + resultado)
+// let resultado = 10%5 == 0 ? "PAR" : "ÍMPAR"
+// console.log("O número que está sendo manipulado é :" + resultado)
+
+
+//Declarando um array
+// let nomes = ["nome1", "nome2", "nome3"]
+
+// console.log(nomes)
+// console.table(nomes)
+
+//IMPRIMIR APENAS UM ITEM DO ARRAY
+// console.log(nomes[1])
+
+//getElementByTagName
+// const itemsDeLista = document.getElementsByTagName("li")
+
+// let num1 = [10,20,30]
+// let num2 = [40,50,60,70,80]
+// let num3
+
+// num3 = [num1,num2]
+// //Apresentando Interpolação junto com arrays
+// console.log("Valor do array 3: " + num3)
