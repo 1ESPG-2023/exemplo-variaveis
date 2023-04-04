e577915002aa21a7704501c381a42a7d41baab54
RodriguesEric134 <rm550249@fiap.com.br>
2023-04-04 11:01:28 -0300

    declaração arrays + getElementByTagName

--- rm550249/js/script.js
+++ rm550249/js/script.js
@@ -1,4 +1,4 @@
-//capturando a tag com id e colocando em uma variável var
+/*//capturando a tag com id e colocando em uma variável var
 var h1Elemento = document.getElementById("titulo-1")
 
 //Adicionando um listener de Evento
@@ -28,9 +28,25 @@ else(
 )
 
 console.log(nome)*/
+/*
 let num1 = 10;
 let num2 = 5;
 
 //crie uma validação ternária onde resulte no valor de PAR OU ÍMPAR
 let resultado = !(10%5) ? "PAR" : "IMPAR";
-console.log("O número que está sendo manipulado é :" + resultado)
+console.log("O número que está sendo manipulado é :" + resultado)*/
+// let nomes = ["Eric","João", "Matheus", "Fernando", "Rodrigo"]
+// console.table(nomes)
+// console.log(nomes[0])
+
+
+//recuperando elementos de lista através da função
+//getElementByTagName
+
+// const ItensDeLista = document.getElementsByTagName("li")
+let num1 = [10, 20, 30]
+let num2 = [40, 50, 60, 70, 80]
+let num3 
+num3 = [num1,num2] 
+//apresentando interpolação junto com arrays
+console.log("Valor do Array 3 " + num3)
