09def15c48ce0a2a1401ad516a1385b4c5649631
hellen-dev <rm98284@fiap.com.br>
2023-04-04 11:34:08 -0300

    Array com loop forEach e SPREAD.

--- pf0670/js/script.js
+++ pf0670/js/script.js
@@ -14,24 +14,37 @@ h1Elemento.addEventListener("click", function(){
 
 var nome  = "Cova"
 
-if(nome != "") {
-    var nome = "Emanuelle"
-}
+// if(nome != "") {
+//     var nome = "Emanuelle"
+// }
 
 //printing the variable value
 console.log(nome)
 
 
-// recuperando elementos de lista através da função
-// getElementByTagName
 
-// const itensDeLista = document.getElementsByTagName("li")
-
+//realizando a junção de arrays com a função SPREAD ( ... )
 // let num1 = [10, 20, 30]
 // let num2 = [40, 50, 60, 70, 80]
 // let num3
 
-// num3 = [num1, num2]
+// num3 = [...num1, ...num2]
 // //presenting 'interpolação' w/ arrays
 // console.log("Valor do array 3: " + num3)
 
+
+// recuperando elementos de lista através da função
+// getElementByTagName
+
+const itensDeLista = [...document.getElementsByTagName("li")]
+
+// const obj1Array = [...itensDeLista]
+
+// console.log(itensDeLista)
+// console.log(obj1Array)
+
+itensDeLista.forEach(item => {
+    console.log(`ITEM DA LISTA: ${item.textContent}`)
+})
+
+console.log(itensDeLista[0].innerHTML)
