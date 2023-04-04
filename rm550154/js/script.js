58149de15dffc299fadd9c6c8efb749897f5d78b
KevinBeykam <rm550154@fiap.com.br>
2023-04-04 11:53:28 -0300

    FOR IN

---
+++ rm550154/js/script.js
@@ -0,0 +1,125 @@
+
+// //Capturando a tag com ID e colocando em uma variável var
+// const h1Elemento = document.getElementById("titulo-1")
+
+// //Adicionando um Listener de Evento
+// h1Elemento.addEventListener("click",function(){
+
+//     alert("Agora sou autonômo!")
+//     h1Elemento.innerHTML = "NOVO TÍTULO!";
+//     h1Elemento.style.backgroundColor = "#ff0000"
+// })
+
+// h1Elemento.addEventListener("mouseover", ()=>{
+//     h1Elemento.style.fontSize = "68px"
+// })
+
+// h1Elemento.addEventListener("mouseleave", ()=>{
+//     h1Elemento.style.fontSize = "34px"
+// })
+
+// //Declaração de variáveis com LET e VAR e CONST
+
+// //const nome = "Pedro"
+
+// var nome = "Pedro"
+
+// if(nome != ""){
+//     var nome = "Emanuelle"
+// }
+
+// //Imprimindo o valor da variável
+// console.log(nome)
+
+//Estrutura de decisão IF / else / else if
+//Operadores lógicos
+// && AND
+// || OR
+// ! NOT
+// != NOT / DIFERENTE
+
+// let nome = null
+// console.log(nome)
+
+// if (nome != "" && nome != undefined) {
+//     console.log("A variável está preenchida!")
+// }else if(nome == null){
+//     console.log("A variável é null !")
+// }
+// else{
+//     console.log("A variável não está preenchida!")
+// }
+
+//OPERADOR TERNÁRIO ( ? )
+
+// let num1 = 10;
+// let num2 = 5;
+
+// //Crie um validação ternária onde resulte no valor de PAR ou IMPAR!
+
+// let resultado = !(10%5) ? "PAR" : "IMPAR";
+
+// console.log("O nÚmero que está sendo manipulado é : " + resultado)
+
+
+// //Declarando um array
+// let nomes = ["Ana","Pedro","Cauã"]
+
+// console.log(nomes)
+// console.table(nomes)
+
+// //IMPRIMIR APENAS UM ITEM DO ARRAY
+// console.log(nomes[1])
+
+//Recuperando elementos de lista através da função
+//getElementByTagName
+//const itemsDeLista = document.getElementsByTagName("li")
+
+// let num1 = [10,20,30]
+// let num2 = [40,50,60,70,80]
+// let num3
+
+// num3 = [num1,num2]
+// //Apresentando interpolação junto com arrays
+// console.log("Valor do array 3 " + num3) 
+
+
+// //Realizando a junção de rrays com o opoerador SPREAD ( ... )
+// let num1 = [10,20,30]
+// let num2 = [40,50,60,70,80]
+// let num3
+
+// num3 = [...num1,...num2]
+// //Apresentando interpolação junto com arrays
+// console.log("Valor do array 3 " + num3) 
+
+//Recuperando elementos de lista através da função
+//getElementByTagName
+// const itemsDeLista = [...document.getElementsByTagName("li")]
+
+// const obj1Array = [...itemsDeLista]
+
+//LOOP forEach
+// itemsDeLista.forEach( item =>{
+
+//     console.log(`ITEM DA LISTA :${item.textContent}`)
+//     // console.log(itemsDeLista[0].textContent)
+// })
+
+// const itemsDeLista = [...document.getElementsByTagName("li")]
+// //Loop for
+// for (let index = 0; index < itemsDeLista.length; index++) {
+//     console.log(`Itens da lista : ${itemsDeLista[index].textContent}`)
+// }
+
+// const itemsDeLista = [...document.getElementsByTagName("li")]
+// //Loop FOR OF
+// for (const item of itemsDeLista) {
+//     console.log(`Itens da lista com ForOf : ${item.textContent}`)
+// }
+
+const itemsDeLista = [...document.getElementsByTagName("li")]
+//FOR IN
+for (const indice in itemsDeLista) {
+    console.log(`Itens da lista con ForIN ${itemsDeLista[indice].textContent}`)
+}
