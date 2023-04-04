95b4e56b81cbb526ea2546b3aea8398eaec906a5
GabrielMachadCP <rm99880@fiap.com.br>
2023-04-04 11:00:29 -0300

    Declaração de arrays e getElementByTagName

--- 1rm99880/Js/script.js
+++ 1rm99880/Js/script.js
@@ -7,3 +7,25 @@
 // let resultado = !(10%5) ? "PAR" : "ÍMPAR";
 // console.log("O número que está sendo manipulado é: "+ resultado)
 
+// Declarando um array
+
+// let nomes = ["Ana", "Pedro", "Cauã"];
+
+// console.log(nomes);
+// console.table(nomes);
+
+// //Imprimir apenas um item do array
+// console.log(nomes[1])
+
+
+//Recuperando elementos de lista através da função
+//getElementByTagName
+//const itensDeLista = document.getElementsByTagName("li")
+
+let num1 = [10,20,30 ];
+let num2 = [40,50,60,70,80];
+let num3
+
+num3 = [num1,num2]
+//Apresentado interpolação junto com arrays
+console.log("Valor do array 3" + num3)
