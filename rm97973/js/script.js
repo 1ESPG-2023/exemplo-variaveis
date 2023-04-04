2ac3f4b3fb96689da6055431227c6afabf911af9
emnuelle <rm97973@fiap.com.br>
2023-04-04 11:00:28 -0300

    declaração arrays + getelementbytagname

--- rm97973/js/script.js
+++ rm97973/js/script.js
@@ -71,6 +71,25 @@
 //  nome [2]
 
 // declarando um array
-let nomes = ["nome1","nome2","nome3"];
+let nomes = ["Ana","Pedro","Cauã"];
 
 console.log(nomes)
+console.table(nomes)
+
+// impriir apenas um intem do array 
+console.log(nomes[1])
+
+// função que pega uma coleção de elementos
+
+// recuperando elementos de lista através da função gitelementbytagname
+// const itensDeLista = document.getElementsByTagName("li")
+
+let num1 = [10,20,30]
+let num2 = [40,50,60,70,80]
+let num3 
+
+num3 = [num1,num2]
+
+// apresentando interpolação junto com arrays
+
+console.log("Valor do array 3 " + num3)
