e41eb0fa59e94c0c89a2fe6a5cf35053a2f99ba6
anazerlin <rm98065@fiap.com>
2023-04-04 11:26:05 -0300

    interpolação de string

--- pf0670/js/script.js
+++ pf0670/js/script.js
@@ -9,13 +9,24 @@
 
 //recuperando elementos de listas atraves da função
 //getElementByTagName
-//const itemsDeLista = document.getElementsByTagName("li")
 
+
+const itemsDeLista = document.getElementsByTagName("li")
+
+//const obj1Array = [...itemsDeLista]
+
+itemsDeLista.forEach(item =>{
+    console.log("`item da lista:${item}`")
+    //console.log(itemsDeLista[0].innerHTML)
+})
+
+
+//realizando a junção de arrays com o operador SPREAD
 // let num1 = [10, 20, 30]
 // let num2 = [40, 50, 60, 70, 80]
 // let num3 
 
-// num3 = [num1, num2]
+// num3 = [...num1, ...num2]
 
 // //apresentando interpolação junto com arrays
 // console.log("valor do array 3" + num3)
