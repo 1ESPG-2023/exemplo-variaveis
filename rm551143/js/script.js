525b866b2f006cc034078c22f9739a9b61f74649
LanaAuada <rm551143@fiap.com.br>
2023-04-04 11:02:18 -0300

    declaração arrays + getElementByTagName

--- rm551143/js/script.js
+++ rm551143/js/script.js
@@ -57,11 +57,36 @@ console.log("A variável não está preenchida")
 
 //operador ternário
 
-let num1 = 10;
-let num2 = 5;
+//let num1 = 10;
+//let num2 = 5;
 
 //crie uma validação ternária onde resulte no valor de PAR ou ÍMPAR
 //let resultado = 10%5 ?"PAR" : "ÍMPAR";
 // lê como positivo logo se resultado é 0 dá ímpar(false)
-let resultado = !(10%5) ?"PAR" : "ÍMPAR";
-console.log("O número que esá sendo manipulado é:" + resultado)
+//let resultado = !(10%5) ?"PAR" : "ÍMPAR";
+//console.log("O número que esá sendo manipulado é:" + resultado)
+
+//declarando um array
+/*let nomes = ["nome1", "nome2", "nome3"]
+console.log(nomes)
+console.table(nomes)
+//imprimir apenas um item do array
+console.log(nomes[1])*/
+// let nomes = ["geovanna", "fernanda", "mirella"]
+// console.log(nomes)
+// console.table(nomes)
+// //imprimir apenas um item do array
+// console.log(nomes[1])
+
+//recuperando elementos e lista através de função
+//getElementsByTagName
+// const itensDeLista = document.getElementsByTagName("li")
+
+// let num1 = [10, 20, 30]
+// let num2 = [40, 50, 60, 70 ,80]
+// let num3
+
+// num3 = [num1,num2]
+// //apresentando interpolação junto com arrays
+// console.log("valor do array 3 "  + num3)
+
