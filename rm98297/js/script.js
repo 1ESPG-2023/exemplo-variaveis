cc96a1f955b139fc11ccd3126cfb69f258844d79
lucassperanzini <RM98297@fiap.com.br>
2023-04-04 11:03:18 -0300

    declaração arryays e + getElementByTagName

--- rm98297/js/script.js
+++ rm98297/js/script.js
@@ -63,14 +63,36 @@
 
 // //Operador ternário
 
-let num1 = 10;
-let num2 = 5
+// let num1 = 10;
+// let num2 = 5
 
 //Crie uma validação pe=ternária em que resulte no valor de par ou ímpar
 
-let resultado = !(10 % 5) ?  "PAR" : "ÍMPAR" ;
+// let resultado = !(10 % 5) ?  "PAR" : "ÍMPAR" ;
 
-    console.log("O número que está sendo manipulado é : " + resultado)
+//     console.log("O número que está sendo manipulado é : " + resultado)
     
 
+// let nomes = ["Ana","Pedro","Cauã"]
 
+
+// console.table(nomes)
+
+
+// // imprmir apenas um item do array
+
+// console.log(nomes[1])
+
+
+//Recuperando elementos de lista através da função
+// getElementByTagName
+//const itemsDeLista = document.getElementsByTagName("li")
+
+
+// let num1 = [10,20,30]
+// let num2 = [40,50,60,70,80]
+// let num3
+
+// num3 = [num1, num2]
+// // apresentando interpolação junto com arrays
+// console.log(" Valor do array 3" +" " + num3)
