06362a7e6734d38481b329572c7c4f914e6e52f5
FernandoPLP <rm99919@fiap.com.br>
2023-04-04 10:38:58 -0300

    declarando o array e o console.table

--- rm99919/js/script.js
+++ rm99919/js/script.js
@@ -1,78 +1,69 @@
-// const h1Elemento = document.getElementById("titulo-1") 
+// // const h1Elemento = document.getElementById("titulo-1") 
 
-//adicionando um listener evenet 
-// h1Elemento.addEventListener("click", function(){ 
-//     alert("Alerta!" );vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv 
-//     h1Elemento.innerHTML = "Novo titulo";vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv 
-//     h1Elemento.style.backgroundColor = "#ff0000" 
-// }) 
+// //adicionando um listener evenet 
+// // h1Elemento.addEventListener("click", function(){ 
+// //     alert("Alerta!" );
+// //     h1Elemento.innerHTML = "Novo titulo"; 
+// //     h1Elemento.style.backgroundColor = "#ff0000" 
+// // }) 
 
-// h1Elemento.addEventListener("mouseover", function(){ 
-//     h1Elemento.style.fontSize = "58px" 
-// }) 
+// // h1Elemento.addEventListener("mouseover", function(){ 
+// //     h1Elemento.style.fontSize = "58px" 
+// // }) 
 
-  
+// // h1Elemento.addEventListener("mouseleave", function(){ 
+// //     h1Elemento.style.fontSize = "34px" 
+// // }) 
 
-// h1Elemento.addEventListener("mouseleave", function(){ 
-//     h1Elemento.style.fontSize = "34px" 
-// }) 
+// // var nome = "Fernando" 
+// //let nome1 = "Fernando" 
+// //const nome2 = "Fernando" 
+// // if(nome != ""){ 
+// //     var nome="Pedro" 
+// //     let nome1="Pedro" 
+// //     const nome2="Pedro" 
+// //     console.log(nome) 
+// //     console.log(nome1) 
+// //     console.log(nome2) 
+// // } 
 
+// // console.log(nome) 
+// // console.log(nome1) 
+// // console.log(nome2) 
 
-// var nome = "Fernando" 
-//let nome1 = "Fernando" 
-//const nome2 = "Fernando" 
-// if(nome != ""){ 
-//     var nome="Pedro" 
-//     let nome1="Pedro" 
-//     const nome2="Pedro" 
-//     console.log(nome) 
-//     console.log(nome1) 
-//     console.log(nome2) 
-// } 
+// //estrutura de decisão if / else / elif 
+// //operadores lógicos  
+// // &&and  
+// // != no 
+// // ||or 
 
-  
+// // let nome =null 
+// // if (nome != "" && nome!= undefined ) { 
+// //     console.log("A variavel está preenchida") 
+// // }else if(nome == null){ 
+// //     console.log("A variavel é null") 
+// // } 
+// // else{ 
+// //     console.log("A variavel está vazia") 
+// // } 
 
-// console.log(nome) 
-// console.log(nome1) 
-// console.log(nome2) 
+// OPERADORES RELACIONAIS 
+// > MAIOR 
+// < MENOR 
+// >= MAIOR OU IGUAL 
+// <= MENOR OU IGUAL 
+// != DIFERENTE 
+// == IGUAL 
+// */ 
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////// 
+// /*OPERADOR TERNARIO*/ 
+// let num= 10 ; 
+// let num2 = 5;   
+// //crie uma validacao ternario onde resulte no valor de par ou impar 
+// let resultado = !(10%5) ? "Par" : "Impar" 
+// console.log("O numero que está sendo manipulado é: " + resultado) 
 
-//estrutura de decisão if / else / elif 
-//operadores lógicos  
-// &&and  
-// != no 
-// ||or 
+//declarando o array
+let nome = ["nome1", "nome2", "nome3"]
 
-  
-
-// let nome =null 
-// if (nome != "" && nome!= undefined ) { 
-//     console.log("A variavel está preenchida") 
-// }else if(nome == null){ 
-//     console.log("A variavel é null") 
-// } 
-// else{ 
-//     console.log("A variavel está vazia") 
-// } 
-
-  
-
-/* 
-OPERADORES RELACIONAIS 
-> MAIOR 
-< MENOR 
->= MAIOR OU IGUAL 
-<= MENOR OU IGUAL 
-!= DIFERENTE 
-== IGUAL 
-*/ 
-
-  
-
-/*OPERADOR TERNARIO*/ 
-let num= 10 ; 
-let num2 = 5;   
-//crie uma validacao ternario onde resulte no valor de par ou impar 
-let resultado = !(10%5) ? "Par" : "Impar" 
-console.log("O numero que está sendo manipulado é: " + resultado) 
+console.table(nome)
