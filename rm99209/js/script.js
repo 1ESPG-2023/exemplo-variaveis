1009c086c80854b02737a9b3593b0e82bbc2b512
BernardoPRocha <RM99209@fiap.com.br>
2023-04-04 11:34:13 -0300

    Array com loop forEach e SPREAD

--- rm99209/js/script.js
+++ rm99209/js/script.js
@@ -11,10 +11,26 @@
 //getElementByTagName
 //const itemsDeLista = document.getElementsByTagName("li");
 
-// let num1 = [10,20,30]
-// let num2 = [40,50,60,70,80]
-// let num3 
 
-// num3 = [num1,num2]
-// //APRESENTANDO INTERPOLAÇÃO JUNTO COM ARRAYS
-// console.log("Valor do array 3 " + num3)
+// //REALIZANDO A JUNÇÃO DE ARRAYS COM O OPERADOR SPREAD ( ... )
+//  let num1 = [10,20,30]
+//  let num2 = [40,50,60,70,80]
+//  let num3 
+
+//  num3 = [...num1,...num2]
+//  //APRESENTANDO INTERPOLAÇÃO JUNTO COM ARRAYS
+//  console.log("Valor do array 3: " + num3)
+
+//RECUPERANDO ELEMENTOS DE LISTA ATRAVÉS DA FUNÇÃO 
+//getElementByTagName
+const itemsDeLista = [...document.getElementsByTagName("li")]
+
+//const objArray = [...itemsDeLista]
+
+itemsDeLista.forEach( item =>{
+   
+   console.log(`ITEM DA LISTA :${item.textContent}`)
+   //console.log(itemsDeLista[0].textContent)
+})
+
+
