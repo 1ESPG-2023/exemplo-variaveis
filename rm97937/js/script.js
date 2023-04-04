ed9dab0a450448ce320c05b951a821ef18714683
Petrus369 <rm97937@fiap.com.br>
2023-04-04 11:01:39 -0300

    Declaração Arrays e getElementsByTypeName.

--- rm97937/js/script.js
+++ rm97937/js/script.js
@@ -31,28 +31,51 @@
 //Estrutura de decisão if - else - else if
 //Operadores lógicos: && => and, || => or, ! => not, != => not equal
 
-let nome = null
-console.log(nome)
-if(nome != "" && nome != undefined) {
-    console.log("A variável está preenchida")
-}
+// let nome = null
+// console.log(nome)
+// if(nome != "" && nome != undefined) {
+//     console.log("A variável está preenchida")
+// }
 
-else if(nome == null) {
-    console.log("A variável é null")
-}
+// else if(nome == null) {
+//     console.log("A variável é null")
+// }
 
-else {
-    console.log("A variável não está preenchida")
-}
+// else {
+//     console.log("A variável não está preenchida")
+// }
 
-//Operadores relacionais são os mesmos que os do python.
+// //Operadores relacionais são os mesmos que os do python.
 
-//Operador ternário: (?)
+// //Operador ternário: (?)
 
-let num1 = 10;
-let num2 = 5;
+// let num1 = 10;
+// let num2 = 5;
 
-//Criando uma validação ternária onde resulte no valor de PAR ou IMPAR:
+// //Criando uma validação ternária onde resulte no valor de PAR ou IMPAR:
 
-let resultado = !(10%5) ? "Par" : "Impar";
-console.log("O número que está sendo manipulado é " + resultado)
+// let resultado = !(10%5) ? "Par" : "Impar";
+// console.log("O número que está sendo manipulado é " + resultado)
+
+// // Declarando um Array:
+
+// let nomes = ["Ana", "Pedro", "Cauã"];
+// console.log(nomes);
+// console.table(nomes);
+
+// //Imprimindo apenas um item do Array:
+
+// console.log(nomes[1]);
+
+//Recuperando elementos de lista através da função getElementByTagName
+//Assinatura é o nome da função
+//const itensDeLista = document.getElementsByTagName("li")
+
+// let num1 = [10, 20, 30,]
+// let num2 = [40, 50, 60, 70, 80]
+// let num3
+
+// num3 = [num1, num2]
+
+// //Apresentando interpolação junto com Arrays:
+// console.log("Valor do Array 3 " + num3)
