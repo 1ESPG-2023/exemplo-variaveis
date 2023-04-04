fbb1db02c8dee8331b71eb5ffd95fea2a83d9c20
Rodrigooou <rm550816@fiap.com.br>
2023-04-04 11:01:51 -0300

    COMITADOLINDO

--- rm550816/js/script.js
+++ rm550816/js/script.js
@@ -1,60 +1,82 @@
 
 
-//Capturando a tag com ID e colocando em uma variável var
-var h1Elemento = document.getElementById("titulo-1")
+// //Capturando a tag com ID e colocando em uma variável var
+// var h1Elemento = document.getElementById("titulo-1")
 
-//Adicionando um Listener de Evento
-h1Elemento.addEventListener("click", function(){
-
-
-  alert("Agora sou autonomo")
-  h1Elemento.innerHTML="NOVIDADES";
-   h1Elemento.style.backgroundColor = "#ff0000"
+// //Adicionando um Listener de Evento
+// h1Elemento.addEventListener("click", function(){
 
 
-})
-
-h1Elemento.addEventListener("mouseover", ()=>{
-   h1Elemento.style.fontSize = "68px"
-})
-h1Elemento.addEventListener("mouseleave", ()=>{
-   h1Elemento.style.fontSize = "32px"
-})
-
-//Declaração de variáveis com LET e VAR e CONST
-var nome = "Pedro"
-
-if(nome != ""){
-   var name ="rodrigo"
-}
-console.log(name)
-
-//Estrutura de decisão IF / else /else if
-//Operadores lógicos
-//&& AND
-//|| OR
-//! NOT
-//!= NOT/DIFERENTE
+//   alert("Agora sou autonomo")
+//   h1Elemento.innerHTML="NOVIDADES";
+//    h1Elemento.style.backgroundColor = "#ff0000"
 
 
-let nome = null
-console.log(name)
-if (nome != "" && nome !="undefined"){
-    console.log("A variável está preenchida!")
-}else if(nome == null){
-    console.log("A variável é null")
-}
-else{
-    console.log("A variável não está preenchida")
-}
+// })
 
-//Operador Ternário?
+// h1Elemento.addEventListener("mouseover", ()=>{
+//    h1Elemento.style.fontSize = "68px"
+// })
+// h1Elemento.addEventListener("mouseleave", ()=>{
+//    h1Elemento.style.fontSize = "32px"
+// })
 
-let num1 = 10;
-let num2 = 5;
+// //Declaração de variáveis com LET e VAR e CONST
+// var nome = "Pedro"
 
-//Crie uma validação ternária onde resulte no novo valor do PAR ou IMPAR:
-//%divide
+// if(nome != ""){
+//    var name ="rodrigo"
+// }
+// console.log(name)
 
-let resultado = !(10%5) ? "PAR" : "IMPAR";
-console.log("O número que está sendo manipulado: " + resultado)
+// //Estrutura de decisão IF / else /else if
+// //Operadores lógicos
+// //&& AND
+// //|| OR
+// //! NOT
+// //!= NOT/DIFERENTE
+
+
+// let nome = null
+// console.log(name)
+// if (nome != "" && nome !="undefined"){
+//     console.log("A variável está preenchida!")
+// }else if(nome == null){
+//     console.log("A variável é null")
+// }
+// else{
+//     console.log("A variável não está preenchida")
+// }
+
+// //Operador Ternário?
+
+// let num1 = 10;
+// let num2 = 5;
+
+// //Crie uma validação ternária onde resulte no novo valor do PAR ou IMPAR:
+// //%divide
+
+// let resultado = !(10%5) ? "PAR" : "IMPAR";
+// console.log("O número que está sendo manipulado: " + resultado)
+
+
+////Declarando um array
+//let nomes = ["Rodrigo", "Jão", "Fernando", "Matheus", "Eric"];
+//console.log(nomes);
+//console.table(nomes);
+
+//Imprimir apenas um item do array
+//console.log=([1])
+
+////Recuperando elementos de lista através da função
+////getElementByTagName
+//const ItemsDelista = document.getElementsByTagName("li")
+
+let num1 = [10, 20, 30];
+let num2 = [40, 50, 60, 70, 80];
+let num3
+
+num3 = [num1 , num2]
+
+//Apresentando Iterpolação junto com arrays
+console.log("Valor do array 3 " + num3)
